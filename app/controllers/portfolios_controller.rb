4eb9dfeb065a0c61887c3568e1483e0f925452ae
zainudinsainal <supernova37@gmail.com>
2017-11-24 21:59:25 +0800

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,37 +1,40 @@
 class PortfoliosController < ApplicationController
     def index
-        @portfolio_items = Portfolio.all
+      @portfolio_items = Portfolio.all
     end
 
     def new
-        @portfolio_item = Portfolio.new
+      @portfolio_item = Portfolio.new
     end
 
     def create
-        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-    
-        respond_to do |format|
-          if @portfolio_item.save
-            format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
-          else
-            format.html { render :new }
-          end
+      @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  
+      respond_to do |format|
+        if @portfolio_item.save
+          format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        else
+          format.html { render :new }
         end
-    end
+      end
+  end
 
     def edit
-        @portfolio_item = Portfolio.find(params[:id])
+      @portfolio_item = Portfolio.find(params[:id])
     end
 
     def update
-        @portfolio_item = Portfolio.find(params[:id])
+      @portfolio_item = Portfolio.find(params[:id])
+      respond_to do |format|
+        if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+          format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+        else
+          format.html { render :edit }
+        end        
+      end
+    end
 
-        respond_to do |format|
-          if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
-            format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
-          else
-            format.html { render :edit }
-          end
-        end
+    def show
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
