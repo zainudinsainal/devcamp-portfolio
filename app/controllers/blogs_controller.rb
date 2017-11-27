89475655986ce912ff02d6d90b1474e823361030
zainudinsainal <supernova37@gmail.com>
2017-11-27 11:46:43 +0800

    Integrated friendly routes for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -60,8 +60,8 @@ class BlogsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
-    end
+      @blog = Blog.friendly.find(params[:id])
+end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
