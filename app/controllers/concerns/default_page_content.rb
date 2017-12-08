a53a69ddf7a9d8542bad4a2a6136d330cf7a1eec
zainudinsainal <supernova37@gmail.com>
2017-12-08 10:57:52 +0800

    Change SEO

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,6 +7,6 @@ module DefaultPageContent
   
     def set_page_defaults
       @page_title = "Devcamp Portfolio | My Portfolio Website"
-      @seo_keywords = "Jordan Hudgens portfolio"
+      @seo_keywords = "Zainudin portfolio"
     end
   end
