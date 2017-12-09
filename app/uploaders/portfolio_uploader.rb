0815636d0a2dcde3e0da87f2414149ac7d53d0bb
zainudinsainal <supernova37@gmail.com>
2017-12-09 16:09:20 +0800

    Finished carrierwave installation

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,6 +1,6 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
 
-  storage :file
+  storage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -11,5 +11,5 @@ class PortfolioUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
-  
+
 end
