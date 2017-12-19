447aa71e133b0f17311c363a099644ff7fa6de7b
zainudinsainal <supernova37@gmail.com>
2017-12-19 16:18:15 +0800

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "zikrullah",
+  password_confirmation: "zikrullah",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "zikrullah",
+  password_confirmation: "zikrullah",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
     Topic.create!(
       title: "Topic #{topic}"
